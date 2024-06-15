ebaf6d14c47f6853e0a33e48938feae7f60b5805
cletoxWTF <jolallaq@est.ups.edu.ec>
2024-06-14 23:48:57 -0500

    Implementación de ruta Pagos
    Se agrega el endpoint de Pagos

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+<<<<<<< Updated upstream
 from controllers import user_controller, role_controller, permission_controller,facturacion_controller
+=======
+from controllers import user_controller, role_controller, permission_controller,reserva_controller, facturacion_controller, pagos_controller
+>>>>>>> Stashed changes
 app = FastAPI()
 
 app.include_router(user_controller.router, prefix="/api/v1")
 app.include_router(role_controller.router, prefix="/api/v1")
 app.include_router(permission_controller.router, prefix="/api/v1")
+<<<<<<< Updated upstream
 app.include_router(facturacion_controller.router, prefix="/api/v1")
+=======
+app.include_router(reserva_controller.router, prefix="/api/v1")
+app.include_router(facturacion_controller.router, prefix="/api/v1")
+app.include_router(pagos_controller.router, prefix="/api/v1")
+>>>>>>> Stashed changes
